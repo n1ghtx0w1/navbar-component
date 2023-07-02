8f7da840e6f7cb4107a5ca983c65d81a125d5a64
n1ghtx0w1 <rhead13@wgu.edu>
2023-07-01 21:55:41 -0400

    Added hamburger response menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <Router>
       <nav className="navbar">
-      <div className="nav-brand">Your-Project</div> {/* Change brand */}
-        <NavLink exact to="/" activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink to="/news" activeClassName="active">
-          News
-        </NavLink>
-        <NavLink to="/blog" activeClassName="active">
-          Blog
-        </NavLink>
-        <NavLink to="/about" activeClassName="active">
-          About
-        </NavLink>
+        <div className="nav-brand">Your-Project</div>
+        <div className={`menu-icon ${showMenu ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={`nav-links ${showMenu ? 'open' : ''}`}>
+          <NavLink exact to="/" activeClassName="active" onClick={toggleMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/news" activeClassName="active" onClick={toggleMenu}>
+            News
+          </NavLink>
+          <NavLink to="/blog" activeClassName="active" onClick={toggleMenu}>
+            Blog
+          </NavLink>
+          <NavLink to="/about" activeClassName="active" onClick={toggleMenu}>
+            About
+          </NavLink>
+        </div>
       </nav>
     </Router>
   );
